549734ba606a84b07aab611f468ac18d4d478b2b
0820 <zq272@nyu.edu>
2016-03-18 21:10:06 -0400

    Host fonts locally and edit layout of main menu

--- wp-content/themes/Tourini/functions.php
+++ wp-content/themes/Tourini/functions.php
@@ -120,9 +120,9 @@ function tourini_scripts() {
 
 	wp_enqueue_script( 'tourini-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	
-	// Add more fonts from google
-	wp_enqueue_script( 'tourini-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic|Merriweather:400,400italic,700,700italic' );
-	
+	// Add local fonts downloaded from google
+	wp_enqueue_style( 'tourini-local-fonts', get_template_directory_uri() . '/fonts/custom-fonts.css' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
